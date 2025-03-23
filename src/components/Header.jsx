85c41c1edb452b4fd26428a34db0314b66c1f0f2
Allen Douglas <allenissabigboi@gmail.com>
2025-03-23 11:12:20 -0700

    Add Header component with user icon and alert functionality; update dependencies and styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from "react";
+import { FaUserCircle } from "react-icons/fa";
 
 const Header = () => {
+    const handleClick = () => {
+        alert("Hello")
+    };
+
+    
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <header className="header">
+      <div>
+        <img src="" alt="logo" /> 
+      </div>
+      <div>
+        <FaUserCircle size={24} onClick={handleClick} />
+      </div>
+    </header>
+  );
+};
 
 export default Header;
-
