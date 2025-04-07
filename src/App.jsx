5c032c5b6d2123757da49241742057bb56e35183
Allen Douglas <allenissabigboi@gmail.com>
2025-04-06 17:27:01 -0700

    fix: correct import case for Header component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import Header from './components/header'
+import Header from './components/Header'
 import MainSection from './components/MainSection'
 import Footer from './components/Footer'
 import './App.css'
